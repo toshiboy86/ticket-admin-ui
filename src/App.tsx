d5f1befd45eed5ed09654ee54289b2539128fb84
toshibowie86@gmail.com <toshibowie86@gmail.com>
2020-10-10 20:32:19 +0900

    Merge conflic resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -44,4 +16,3 @@ const App: React.FC = () => {
 };
 
 export default App;
->>>>>>> Stashed changes
