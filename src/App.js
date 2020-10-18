efcc5cc6355f32d86157d3b13231335946be8b03
toshibowie86@gmail.com <toshibowie86@gmail.com>
2020-10-18 20:15:36 +0900

    Convert to https for test data

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin authProvider={authProvider} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} icon={PostIcon} />
