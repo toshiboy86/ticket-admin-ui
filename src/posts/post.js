198c9fcfe34006728da568d669a49ea341dd2aa3
toshibowie86@gmail.com <toshibowie86@gmail.com>
2020-10-18 20:10:49 +0900

    Remove unnecessary const

--- src/posts/post.js
+++ src/posts/post.js
@@ -1,10 +1,6 @@
 import * as React from "react";
 import { SimpleForm, ReferenceInput, SelectInput, TextInput, Create } from 'react-admin';
 
-const PostTitle = ({ record }) => {
-    return <span>Post {record ? `"${record.title}"` : ''}</span>;
-};
-
 export const PostCreate = props => (
     <Create {...props}>
         <SimpleForm>
